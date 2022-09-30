c28902f4399ff73d248d1c2f0690f45e27204aef
Chika Nwazuo <34070274+Bright11@users.noreply.github.com>
2022-09-30 15:51:39 +0100

    Update index.js

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use("/students", studentRoutes); //importing the student route
 
 const CONNECTION_URL =
-  "mongodb+srv://chika:Myfather@tinderdb.vbmmdul.mongodb.net/test?retryWrites=true&w=majority";
+  "mongodb+srv://chika:{password}@tinderdb.vbmmdul.mongodb.net/{dbname}?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
 mongoose
